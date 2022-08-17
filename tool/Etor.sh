9d8b1d6d939507116e8166e56f60465c09377be0
EtorChecker <111360633+EtorChecker@users.noreply.github.com>
2022-08-17 17:56:48 +1000

    Update Etor.sh

--- tool/Etor.sh
+++ tool/Etor.sh
@@ -2,4 +2,4 @@ PROJECTLOCATION="/mnt/c/Users/User1/Etor/"
 PYTHONLOCATION="/mnt/c/Users/User1/Python/"
 HEAP="-Xms2048m -Xmx2048m" 
 export JAVA_OPTS="$HEAP"
-java -jar Etor.jar $1 $2 $3 $4 $5
+java -jar Etor.jar $1 $2 $3 $4 $5 $PROJECTLOCATION $PYTHONLOCATION
