685f2c7a9c911969869830b0991707efa13c4960
EtorChecker <111360633+EtorChecker@users.noreply.github.com>
2022-08-17 17:55:11 +1000

    Update Etor.sh

--- tool/Etor.sh
+++ tool/Etor.sh
@@ -1,3 +1,5 @@
+PROJECTLOCATION="/mnt/c/Users/User1/Etor/"
+PYTHONLOCATION="/mnt/c/Users/User1/Python/"
 HEAP="-Xms2048m -Xmx2048m" 
 export JAVA_OPTS="$HEAP"
-java -jar ESDroid.jar $1 $2 $PLATFORDIRFORSLICER $SOURCESINK $3
+java -jar Etor.jar $1 $2 $3 $4 $5
